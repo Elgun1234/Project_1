ced703749eae6ca853b1c5b0f6a2818ec5c14beb
Elgun1234 <112864674+Elgun1234@users.noreply.github.com>
2022-11-18 12:42:27 +0000

    Update main.py

--- Project_1/main.py
+++ Project_1/main.py
@@ -1,45 +1,48 @@
 import logging
 import os
 from pathlib import Path
-from typing import Union
-from fastapi import FastAPI, Request
+from typing import Optional
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from Project_1.core.weather import Weather
 
+from Project_1.core.weather import Weather
 
 logs_file = Path(Path().resolve(), "log.txt")
 logs_file.touch(exist_ok=True)
+
 logging.basicConfig(
-format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-datefmt="%Y-%m-%d %H:%M:%S",
-level=os.environ.get("LOGLEVEL", "INFO"),
-handlers=[logging.FileHandler(logs_file), logging.StreamHandler()],
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=os.environ.get("LOGLEVEL", "INFO"),
+    handlers=[logging.FileHandler(logs_file), logging.StreamHandler()],
 )
 
-
-
 log = logging.getLogger(__name__)
+
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-weather_api = Weather()
+weather = Weather()
 
 @app.get("/")
 async def root():
     log.info("gone to root")
     return {"message": "Hello World"}
+
+
 @app.get("/items/{item_id}")
 async def read_item(item_id):
-    log.warning(f"loaded item {item_id}")
+    log.info(f"loaded item {item_id}")
     return {"item_id": item_id}
+
 
 @app.get("/weather")
 def get_weather(latitude: float = 51.5002, longitude: float = -0.120000124, options:str = "temperature_2m"):
     log.info(f"Requested latitude: {latitude} and longitude: {longitude} with options {options}")
-    output = weather_api.get_weather(latitude=latitude, longitude=longitude, options=options)
+    output = weather.get_weather(latitude=latitude, longitude=longitude, options=options)
     return {"weather": output}
 
 
@@ -51,18 +54,28 @@ def html_output(request: Request):
     )
 
 @app.get("/chart", response_class=HTMLResponse)
-def html_output(request: Request):
-    data = get_weather(options="temperature_2m")
-    data = data['weather']['hourly']['time']
+def chart_output(request: Request, latitude: float = 51.5002, longitude: float = -0.120000124, city="London", options:str = "temperature_2m", limit:int=50):
+    data = weather.get_weather(latitude=latitude, longitude=longitude, options=options)
 
-
-
-
+    labels = data['hourly']['time'][:limit]
+    values = data['hourly']['temperature_2m'][:limit]
+    rain_values = data['hourly']['rain'][:limit]
+    visibility_values =data['hourly']['visibility'][:limit]
+    labels = ",".join(labels)
+    values = str(values)[1:-1]
     return templates.TemplateResponse(
         "weather.html",
-        {"request": request, "data": get_weather(options="temperature_2m")},
+        {"request": request, "data": [data, labels, values, rain_values,visibility_values], "city": [city.title(), latitude, longitude]},
     )
 
 @app.post("/weather_send")
-def weather_send(country:str = "london", temperature:str | None = None):
-    return {"country": country, "temp": temperature}
+async def weather_send(request: Request, city:str=Form(),rain: Optional[str] = Form(None),visibility: Optional[str] = Form(None)):
+    cities = {"berlin": [52.52, 13.41], "london": [51.51, -0.13], "paris": [48.8566, 2.3522]}
+    options = "temperature_2m"
+    if rain:
+        options +=",rain"
+    if visibility:
+        options += ",visibility"
+    if city in cities:
+        return chart_output(request=request, latitude=cities[city][0], longitude=cities[city][1], options=options, city=city.title())
+    return chart_output(request=request,options=options, city=city.title())
